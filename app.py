4865d0ce68112748068cd43329fcf419fcd05d41
Aidan Gorny <aidang1003@gmail.com>
2020-10-25 19:19:00 -0500

    Layout
    commit has a working layout and computations. Still have to show the computations on screen

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -7,9 +7,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', pageTitle='Homepage')
 
-@app.route('/aidan')
-def aidan():
-    return render_template('aidan.html')
+@app.route('/monthlyPmt', methods=["POST"])
+def mnthlyPmt():
+    if request.method == "POST":
+        form = request.form
+        loanAmt = float(form['loanAmt'])
+        lengthOfLoan = float(form['lengthOfLoan'])
+        intRate = float(form['intRate'])
+        print('++++++++')
+        print(loanAmt)
+        print(lengthOfLoan)
+        print(intRate)
+        calc = (loanAmt/((1 + intRate)**lengthOfLoan)) / (((1+intRate)**lengthOfLoan)*intRate)
+        print(calc)
+        #return render_template('index.html', display=calc, pageTitle="My Calculator")
+    return render_template('monthlyPayment.html', pageTitle='Monthly Payment')
 
 if __name__ == '__main__':
     app.run(debug=True)
